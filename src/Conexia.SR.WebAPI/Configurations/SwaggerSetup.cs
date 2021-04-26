29b026d9979b89f8eac14661e1100e6a4c030315
Pedro Pinheiro <phleitepinheiro@gmail.com>
2021-04-26 02:00:41 -0300

    feat: add swagger

---
+++ src/Conexia.SR.WebAPI/Configurations/SwaggerSetup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace Conexia.SR.WebAPI.Configurations
+{
+    public static class SwaggerSetup
+    {
+        public static void AddSwaggerSetup(this IServiceCollection services)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+
+                        Title = "Song Recommendations",
+                        Description = @"An API to register a user and recommend songs based
+                                        on hometown current temperature",
+                        Version = "v1",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Pedro Pinheiro",
+                            Url = new Uri("https://www.linkedin.com/in/phinheiro/")
+                        }
+                    });
+            });
+        }
+
+        public static void UseSwaggerSetup(this IApplicationBuilder app)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+    }
+}
--- src/Conexia.SR.WebAPI/Startup.cs
+++ src/Conexia.SR.WebAPI/Startup.cs
@@ -1,9 +1,6 @@
-using Conexia.SR.Application.AutoMapper;
-using Conexia.SR.Data;
 using Conexia.SR.WebAPI.Configurations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,6 +26,8 @@ namespace Conexia.SR.WebAPI
 
             services.AddAutoMapperSetup();
 
+            services.AddSwaggerSetup();
+
             services.RegisterServices();
         }
 
@@ -46,6 +45,8 @@ namespace Conexia.SR.WebAPI
             app.UseAuthentication();
 
             app.UseAuthorization();
+
+            app.UseSwaggerSetup();
 
             app.UseEndpoints(endpoints =>
             {
